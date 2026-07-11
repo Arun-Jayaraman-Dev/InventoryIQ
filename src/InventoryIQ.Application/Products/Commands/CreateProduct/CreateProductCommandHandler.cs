e343946a403b337bea7a41e55e676b4d9270b58b
Arun Jayaraman <81107456+Arun-Jayaraman-Dev@users.noreply.github.com>
2026-07-11 11:16:57 +0530

    feat(application): add CreateProductCommandHandler and UTs (#5)
    * feat: added create product command and repository
    
    * add createproductcommandhandler tests
    
    ---------
    
    Co-authored-by: Arun Jayaraman <sendtoarun.j@gmail.com>

---
+++ src/InventoryIQ.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using InventoryIQ.Application.Products.Commands.CreateProduct.Exceptions;
+using InventoryIQ.Application.Products.Interfaces;
+using InventoryIQ.Domain.Entities;
+
+namespace InventoryIQ.Application.Products.Commands.CreateProduct
+{
+    public sealed class CreateProductCommandHandler
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Guid> Handle(CreateProductCommand command)
+        {
+            var skuExists = await _productRepository.ExistsBySkuAsync(command.Sku);
+
+            if (skuExists)
+            {
+                throw new DuplicateProductSkuException();
+            }
+
+            var product = new Product(command.Name, command.Sku, command.Price, command.Quantity);           
+
+            await _productRepository.AddAsync(product);
+            return product.Id;
+        }
+    }
+}
---
+++ src/InventoryIQ.Application/Products/Commands/CreateProduct/Exceptions/DuplicateProductSkuException.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryIQ.Application.Products.Commands.CreateProduct.Exceptions
+{
+    public class DuplicateProductSkuException: Exception
+    {
+        public DuplicateProductSkuException() : base("Product with the same SKU already exists.")
+        { 
+        }
+    }
+}
